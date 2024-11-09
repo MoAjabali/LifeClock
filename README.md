aefdcd09de948c14e1a31624674cb5303ce40878
Mohammed-Aljablai <mohammed.aljablai@gmail.com>
2024-11-09 09:37:24 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,51 @@
+# Life Clock (Age Calculator App 🧮)
+This project is a simple age calculator. The user enters their date of birth (year, month, and day), and the app calculates their age in years, months, and days. It includes input validation and animated number increments. Click [here]() to visit the website ☺️.
+
+**Note:** This is a solution to the [Age Calculator App Challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/age-calculator-app-dF9DFFpj-Q).
+![Desktop preview](./media/design/desktop-preview.jpg)
+
+## Table of Contents
+
+- [Overview](#overview)
+  - [Challenge Requirements](#challenge-requirements)
+  - [What I Implemented](#what-i-implemented-🤔)
+  - [Screenshots](#screenshots-📸)
+- [Author](#author)
+
+## Overview
+
+### Challenge Requirements 🏳
+> Users should be able to:
+> - View an age in years, months, and days after submitting a valid date through the form.
+> - Receive validation errors if:
+>   - Any field is empty when the form is submitted.
+>   - The day number is not between 1-31.
+>   - The month number is not between 1-12.
+>   - The year is in the future.
+>   - The date is invalid (e.g., 31/04/1991, as April has only 30 days).
+> - View the optimal layout for their device’s screen size.
+> - See hover and focus states for all interactive elements on the page.
+> - **Bonus**: See the age numbers animate to their final value when the form is submitted.
+
+### What I Implemented 🤔
+I implemented all of the above requirements. Additionally, I included input validation both when the calculate button is clicked and when the input field loses focus (this happens after clicking, not before).
+
+### Screenshots 📸
+#### Original Design
+![Original Design](./media/design/desktop-design.jpg)
+
+#### My Implementation 😎
+![My Project Screenshot](./media/images/Screenshot.jpeg)
+
+#### Mobile Design & My Implementation
+![Mobile Design](./media/design/mobile-design.jpg) ![My Mobile Screenshot](./media/images/mobileScreen.jpeg)
+
+**Notes:**
+- I centered my version as I found it more visually appealing.
+- I couldn’t match the exact padding, width, and other dimensions from the design, so my version may look slightly different 🌚.
+
+<!-- ### Links -->
+<!-- - Live Site URL: [Title]() -->
+
+## Author
+- Me 😎 ([@mohammed-Aljablai](https://github.com/mohammed-aljablai))
---
+++ main.js
@@ -0,0 +1,82 @@
+let now = new Date();
+let birthDay= new Date();
+let myButton = document.querySelector("button");
+let day = document.getElementById("inDay");
+let month = document.getElementById("inMonth");
+let year = document.getElementById("inYear");
+let daysOfMonths = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+/* better way for filed
+year.setAttribute("min", 1);
+year.setAttribute("max", now.getFullYear());
+month.setAttribute("min", "1");
+month.setAttribute("max", "12");
+day.setAttribute("min", "1");
+day.setAttribute("max", "31");
+// */
+
+myButton.onclick = ()=>{  
+  validField(year);
+  validField(month);
+  validField(day);
+  
+  let isYearValid = validation(year, now);
+  let isMonthValid = validation(month, 12);
+  let isDayValid = validation(day, daysOfMonths[month.value-1] || 31);
+
+  if ( isYearValid && isMonthValid && isDayValid){
+    birthDay = new Date(year.value, month.value - 1, day.value);
+    let age = now - birthDay;
+    let years = age/1000/60/60/24/365;
+    let months = (years - parseInt(years)) * 12;
+    let days = (months - parseInt(months)) * 30;
+    document.getElementById("yearsNo").innerText = parseInt(years);
+    document.getElementById("monthsNo") .innerText = parseInt(months);
+    document.getElementById("daysNo") .innerText = parseInt(days);
+  }
+  console.log("Have you Clicked me? " + clicked);
+};
+
+// validation
+// i use this long way to check while the user input and when he Click
+function validation(field, biggerThan, smallerThan = 1, wrongEnterMsg = "Please Enter Valid Value", emptyMsg = "Filed is Required"){
+  let valid = false;
+  valid = validationMethods(field, biggerThan, smallerThan, wrongEnterMsg, emptyMsg);
+  field.onblur = ()=>validationMethods(field, biggerThan, smallerThan, wrongEnterMsg, emptyMsg);
+  return valid;
+}
+function validationMethods(field, biggerThan, smallerThan, wrongEnterMsg, emptyMsg){
+  if(field.value =="") 
+    notValidField(field, emptyMsg);  
+  else if(field.value>biggerThan || field.value<smallerThan)
+    notValidField(field, wrongEnterMsg);
+  else {
+    validField(field);
+    return true;
+  }
+  return false;
+}
+
+
+function notValidField(field, msg){
+  let parents = document.querySelector(`.${field.getAttribute("id")} p`);
+
+  // color the filed
+  parents.classList.add("not-valid-color");
+  field.classList.add("not-valid-boarder");
+  document.querySelector(`.${field.getAttribute("id")} label`).classList.add("not-valid-color");
+  
+  // create the element and appended
+  let mySpan = document.createElement("span");
+  mySpan.appendChild(document.createTextNode(msg));
+  parents.appendChild(mySpan);
+  field.onfocus = ()=>{
+    validField(field);
+  }
+}
+function validField(field){
+  let parents = document.querySelector(`.${field.getAttribute("id")} p`);
+  parents.classList.remove("not-valid-color");
+  field.classList.remove("not-valid-boarder");
+  document.querySelector(`.${field.getAttribute("id")} label`).classList.remove("not-valid-color");
+  parents.innerHTML = "";
+}
